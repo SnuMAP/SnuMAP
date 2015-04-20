ff9cc354d5ed5d0cf1439f78d21671a30ab7f6cb
Younghyun <younghyun@csap.snu.ac.kr>
2015-04-20 20:30:50 +0900

    Linux: measure resume/susspend cnt, Profiler: an interface

--- Profiler/sc_profiler.c
+++ Profiler/sc_profiler.c
@@ -1,26 +1,18 @@
 //------------------------------------------------------------------------------
-/// @brief Dynamic RTE profiler implementation
+/// @brief Application trace profiler implementation
 /// @author Younghyun Cho <younghyun@csap.snu.ac.kr>
 /// Computer Systems and Platforms Laboratory
 /// Department of Computer Science and Engineering
 /// Seoul National University
 ///
 /// @section changelog Change Log
-/// 2015/01 Younghyun Cho created @n
-/// 2015/01 Suwon Oh      modified sched_sched_cpuset
-/// 2015/03 Younghyun Cho simple task allocator (using linux load balancer)
-/// 2015/04 Younghyun Cho code clean
+/// 2015/04 Younghyun Cho created @n
 ///
 /// @section license_section Licence
-/// Copyright (c) 2014,2015, Computer Systems and Platforms Laboratory
+/// Copyright (c) 2015, Computer Systems and Platforms Laboratory
 /// All rights reserved.
 /// 
 /// @decription sc_profiler.c
-/// SC-profiler kernel module implementation.
-/// it manages hardware resources for each parallel(OpenMP/OpenCL) applications.
-/// it directly manages each worker thread of parallel applications.
-/// currently, its main functionality is to set cpu core affinities
-/// of each worker thread by taking master thread's pid.
 ///
 //------------------------------------------------------------------------------
 
@@ -32,127 +24,148 @@
 static int profiler_open(struct inode *inode, struct file *file)
 {
 #if ACCESS_ONLY_ONE
-  if (profiler_Open)
-    return -EBUSY;
-  profiler_Open++;
+	if (profiler_Open)
+		return -EBUSY;
+	profiler_Open++;
 #endif
 
-  try_module_get(THIS_MODULE);
-  return SUCCESS;
+	try_module_get(THIS_MODULE);
+	return SUCCESS;
 }
 
 static int profiler_release(struct inode *inode, struct file *file)
 {
 #if ACCESS_ONLY_ONE
-  profiler_opened--;
+	profiler_opened--;
 #endif
-  module_put(THIS_MODULE);
-  return SUCCESS;
+	module_put(THIS_MODULE);
+	return SUCCESS;
 }
 
 static ssize_t profiler_read(struct file *file,
-    char __user *buffer,
-    size_t length,
-    loff_t *offset)
+		char __user *buffer,
+		size_t length,
+		loff_t *offset)
 {
-  return SUCCESS;
+	return SUCCESS;
 }
 
 static ssize_t profiler_write(struct file *file,
-    const char __user *buffer,
-    size_t length,
-    loff_t *offset)
+		const char __user *buffer,
+		size_t length,
+		loff_t *offset)
 {
-  return SUCCESS;
+	return SUCCESS;
+}
+
+void start_profiling(void)
+{
+	struct task_struct* master_thread = current;
+	struct task_struct* task = master_thread;
+
+	do {
+		task->profile_data.starting_flag = 1;
+		task = next_thread(task);
+	} while (task != master_thread);
+}
+
+void stop_profiling(void)
+{
+	struct task_struct* master_thread = current;
+	struct task_struct* task = master_thread;
+
+	do {
+		task->profile_data.starting_flag = 0;
+		task = next_thread(task);
+	} while (task != master_thread);
+}
+
+void dump_profile_result(void)
+{
+	struct task_struct* master_thread = current;
+	struct task_struct* task = master_thread;
+
+	do {
+		int i = 0;
+
+		for (i = 0; i < 64; i++) {
+			printk(KERN_ALERT "task: %d cpu: %d resume: %d suspend: %d\n",
+					task->pid, i, task->profile_data.resume_cnt[i], task->profile_data.suspend_cnt[i]);
+		}
+
+		task = next_thread(task);
+	} while (task != master_thread);
 }
 
 #ifdef UNLOCKED
 int profiler_ioctl(struct file *file,
-                   unsigned int ioctl_num,
-                   unsigned long arg)
+		unsigned int ioctl_num,
+		unsigned long arg)
 #else
 int profiler_ioctl(struct inode *inode, /* see include/linux/fs.h */
-                   struct file *file,
-                   unsigned int ioctl_num,
-                   unsigned long arg)
+		struct file *file,
+		unsigned int ioctl_num,
+		unsigned long arg)
 #endif
 {
-  int ret = 0;
+	int ret = 0;
 
-  switch (ioctl_num) {
-    case IOCTL_COMMAND_1: // IOCTL_START_PROFILING
-    {
-      struct task_struct* task = current;
-      task->profiled_data.starting_flag = 1;
+	switch (ioctl_num) {
+		//case IOCTL_COMMAND_1: // IOCTL_START_PROFILING
+		case 1:
+		{
+			start_profiling();
 
-      break;
-    }
-    case IOCTL_COMMAND_2: // IOCTL_STOP_PROFILING
-    {
-      struct task_struct* task = current;
-      task->profiled_data.starting_flag = 0;
+			break;
+		}
+		//case IOCTL_COMMAND_2: // IOCTL_STOP_PROFILING
+		case 2:
+		{
+			stop_profiling();
 
-      break;
-    }
-    case IOCTL_COMMAND_3: // IOCTL_DUMP_PROFILED_RESULT
-    {
-      struct task_struct* task = current;
-      struct taskprofile_data profiled_data;
-      int i = 0, j = 0;
+			break;
+		}
+		//case IOCTL_COMMAND_3: // IOCTL_DUMP_PROFILED_RESULT
+		case 3:
+		{
+			dump_profile_result();
 
-      for (i=0; i<64; i++) {
-        profiled_data.start_execution_cnt[i] =
-          task->profiled_data.start_execution_cnt[i];
-        profiled_data.end_execution_cnt[i] =
-          task->profiled_data.end_execution_cnt[i];
-      }
+			break;
+		}
+		default:
+		{
+			printk(KERN_ALERT "no support ioctl command %d", ioctl_num);
 
-      for (i=0; i<64; i++) {
-        for (j=0; j<10000; j++) {
-          profiled_data.start_executions[i] =
-            task->profiled_data.start_executions[i][j];
-          profiled_data.end_executions[i] =
-            task->profiled_data.end_executions[i][j];
-        }
-      }
+			break;
+		}
+	}
 
-      ret = copy_to_user((void*)arg, &profiled_data, sizeof(struct taskprofile_data));
-      break;
-    }
-    default:
-    {
-      printk(KERN_ALERT "no support ioctl command %d", ioctl_num);
-
-      break;
-    }
-  }
-
-  return ret;
+	return ret;
 }
 
 int module_start(void)
 {
-  int ret_val;
+	int ret_val;
 
-  /*
-   * Register the character mailbox
-   */
-  ret_val = register_chrdev(MAJOR_NUM, DEVICE_FILE_NAME, &Fops);
-  if (ret_val < 0) {
-    printk(KERN_ALERT "%s failed with %d\n",
-        "Sorry registering the mailbox", ret_val);
-    return ret_val;
-  }
+	/*
+	 * Register the character mailbox
+	 */
+	ret_val = register_chrdev(MAJOR_NUM, DEVICE_FILE_NAME, &Fops);
+	if (ret_val < 0) {
+		printk(KERN_ALERT "%s failed with %d\n",
+				"Sorry registering the mailbox", ret_val);
+		return ret_val;
+	}
 
-  printk(KERN_ALERT "Hello SC profiler!\n");
+	printk(KERN_ALERT "Hello SC profiler!\n");
 
-  return 0;
+	return 0;
 }
 
 void module_end(void)
 {
-  unregister_chrdev(MAJOR_NUM, DEVICE_FILE_NAME);
-  printk(KERN_ALERT "Bye SC profiler!\n");
+	unregister_chrdev(MAJOR_NUM, DEVICE_FILE_NAME);
+	printk(KERN_ALERT "Bye SC profiler!\n");
 }
 
 module_init(module_start);
--- Profiler/sc_profiler.h
+++ Profiler/sc_profiler.h
@@ -1,19 +1,18 @@
 //------------------------------------------------------------------------------
-/// @brief Many-SC profiler (kernel module) header
+/// @brief Application trace profiler header
 /// @author Younghyun Cho <younghyun@csap.snu.ac.kr>
 /// Computer Systems and Platforms Laboratory
 /// Department of Computer Science and Engineering
 /// Seoul National University
 ///
 /// @section changelog Change Log
-/// 2015/01 Younghyun Cho created
+/// 2015/04 Younghyun Cho created
 ///
 /// @section license_section Licence
-/// Copyright (c) 2014,2015 Computer Systems and Platforms Laboratory
+/// Copyright (c) 2015 Computer Systems and Platforms Laboratory
 /// All rights reserved.
 /// 
 /// @decription sc_profiler.h
-/// communication between SC profiler and SC-RTE
 ///
 //------------------------------------------------------------------------------
 
@@ -23,8 +22,63 @@
 #ifndef SC_profiler_H
 #define SC_profiler_H
 
-#include "header.h"
-#include "profiler_interface.h"
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+
+#include <linux/sched.h>
+#include <linux/cpumask.h>
+#include <linux/bitmap.h>
+#include <linux/migrate.h>
+
+#include <linux/smp.h>
+#include <linux/threads.h>
+#include <linux/rcupdate.h>
+#include <linux/cpu.h>
+#include <linux/cpuset.h>
+#include <linux/percpu.h>
+#include <linux/errno.h>
+#include <linux/migrate.h>
+#include <linux/pid_namespace.h>
+
+#include <linux/ioctl.h>
+
+#include <asm/uaccess.h>
+
+#include <linux/mempolicy.h>
+#include <linux/slab.h>
+#include <linux/nodemask.h>
+
+// for checking linux version (ioctl or unlocked_ioctl)
+#include <linux/version.h>
+
+// kernel version dependency
+#if LINUX_VERSION_CODE > KERNEL_VERSION(3,0,0)
+#define UNLOCKED
+#define AMD64
+#elif LINUX_VERSION_CODE > KERNEL_VERSION(2,6,36)
+#define UNLOCKED
+#define TILEGX
+#endif
+
+struct task_struct *find_task_by_pid_ns(pid_t nr, struct pid_namespace *ns);
+struct task_struct *find_task_by_vpid(pid_t vnr);
+struct task_struct *find_process_by_pid(pid_t pid);
+
+#define DEBUG_MODE 0
+#define ACCESS_ONLY_ONE 0
+
+#define IOCTL_START_PROFILING      _IOR(MAJOR_NUM, 1, NULL)
+#define IOCTL_STOP_PROFILING       _IOR(MAJOR_NUM, 2, NULL)
+#define IOCTL_DUMP_PROFILED_RESULT _IOR(MAJOR_NUM, 3, NULL)
+
+#define IOCTL_COMMAND_1 IOCTL_START_PROFILING
+#define IOCTL_COMMAND_2 IOCTL_STOP_PROFILING
+#define IOCTL_COMMAND_3 IOCTL_DUMP_PROFILED_RESULT
+
+#define DEVICE_FILE_NAME "profiler_mailbox"
+#define MAJOR_NUM 101
 
 #define SUCCESS 0
 
@@ -43,6 +97,11 @@ static ssize_t profiler_write(struct file *file,
                               const char __user *buffer,
                               size_t length,
                               loff_t *offset);
+
+/* basic functionalities implementation */
+void start_profiling(void);
+void stop_profiling(void);
+void dump_profile_result(void);
 
 /* kernel module */
 int module_start(void);
