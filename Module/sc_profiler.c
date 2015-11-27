e30f96ef7860ed1266b601b67826d986953e542b
Younghyun Cho <younghyun@csap.snu.ac.kr>
2015-11-27 13:26:51 +0900

    Set OpenMP thread counts by program argument + write elapsed time when dump profiling

--- Module/sc_profiler.c
+++ Module/sc_profiler.c
@@ -36,9 +36,10 @@ int print_taskprofile_list(struct file_write_data* fw_data, int initial_state, i
 
   // list_number == 0 : find first list && initial_state < 0 : ignore first time value
   for (i = ((list_number == 0 && initial_state <0 ) ? 1 : 0); i < tp_current->resume_counts; i++) {
-    print_log(fw_data, "%d, %d, %llu, %llu\n",
+    print_log(fw_data, "%d, %d, %llu, %llu, %llu\n",
         thread_number,
         cpu_number,
+        tp_current->suspend_time[i]-tp_current->resume_time[i],
         tp_current->resume_time[i], 
         tp_current->suspend_time[i]);
   }
@@ -290,7 +291,7 @@ void dump_profile_result(void)
     }
   }
 
-  print_log(fw_data, "thread_number, cpu_number, start_time, end_time\n");
+  print_log(fw_data, "thread_number, cpu_number, execution_time, start_time, end_time\n");
   do {
 		int j = 0;
     //print_log(fw_data, "thread: %d\n", i);
