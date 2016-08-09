e19ecd3e668e9bc2d6fe8085b2d4acdef96d9627
Camilo Celis Guzman <camilo.celis0807@gmail.com>
2016-08-09 13:39:02 +0900

    added changes to Lib directory

--- Lib/OMP_prof.h
+++ Lib/OMP_prof.h
@@ -7,6 +7,7 @@
 ///
 /// @section changelog Change Log
 /// 2015/04 Younghyun Cho created
+/// 2015/10 Heesik Shin apply IOCTL number macro.
 ///
 /// @section license_section Licence
 /// Copyright (c) 2015, Computer Systems and Platforms Laboratory
@@ -27,20 +28,10 @@
 #include <sys/ioctl.h>
 #include <omp.h>
 
-struct taskprofile_data {
-	int resume_cnt[64];
-	int suspend_cnt[64];
-	unsigned long resume_time[64][10000];
-	unsigned long suspend_time[64][10000];
-};
-
+// ioctl definition
 #define IOCTL_START_PROFILING      _IOR(MAJOR_NUM, 1, NULL)
 #define IOCTL_STOP_PROFILING       _IOR(MAJOR_NUM, 2, NULL)
 #define IOCTL_DUMP_PROFILED_RESULT _IOR(MAJOR_NUM, 3, NULL)
-
-#define IOCTL_COMMAND_1 IOCTL_START_PROFILING
-#define IOCTL_COMMAND_2 IOCTL_STOP_PROFILING
-#define IOCTL_COMMAND_3 IOCTL_DUMP_PROFILED_RESULT
 
 #define DEVICE_FILE_NAME "profiler_mailbox"
 #define MAJOR_NUM 101
@@ -54,7 +45,7 @@ int fd;
 int omp_init_profiling(void);
 int omp_start_profiling(void);
 int omp_stop_profiling(void);
-int omp_dump_proflie_result(void);
+int omp_dump_profile_result(void);
 int omp_cleanup_profiling(void);
 
 /// @}
